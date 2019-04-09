56f7ed395df70507f287fb81b513cc53c8382776
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 02:08:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,6 +80,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        :yukihiro_matsumoto => 
+        {
+          :known_for => "Ruby",
+          :languages => ["LIPS", "C"]
+        }
      }
 
     
